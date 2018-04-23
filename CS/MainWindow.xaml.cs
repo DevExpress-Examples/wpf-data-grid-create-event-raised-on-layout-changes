09f7cbb19fa41690110f220122b87cc6a4c02f60
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using DevExpress.Xpf.Grid;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Windows.Input;
+using GridLayoutHelper;
+
+namespace DXGridSample {
+	public partial class MainWindow: Window {
+		ObservableCollection<Person> Persons;
+		public MainWindow() {
+			InitializeComponent();
+			Persons = new ObservableCollection<Person>();
+			for (int i = 0; i < 100; i++)
+				Persons.Add(new Person { Id = i, Name = "Name" + i, Bool = i % 2 == 0 });
+			grid.ItemsSource = Persons;
+		}
+		private void AddColumn_Click(object sender, System.Windows.RoutedEventArgs e) {
+			grid.Columns.Add(new GridColumn() { FieldName = "Name" });
+		}
+		private void RemoveColumn_Click(object sender, System.Windows.RoutedEventArgs e) {
+			grid.Columns.Remove(grid.Columns.Last());
+		}
+		private void GridLayoutHelper_Trigger(object sender, MyEventArgs e) {
+			string str = string.Empty;
+			foreach (var type in e.LayoutChangedTypes)
+				str = string.IsNullOrEmpty(str) ? type.ToString() : str + " | " + type;
+			Debug.WriteLine(str);
+		}
+	}
+	public class Person {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public bool Bool { get; set; }
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,25 @@
+# How to provide an event that is raised on changing a grid layout
+
+
+<p>This example demonstrates how to provide an event that is raised as soon as a grid layout is changed.</p>
+<br />
+<p>In this example, we have created a GridLayoutHelper class that handles changes of the GridControl object's necessary properties and columns, and raises a custom event in its handlers.</p>
+<br />
+<p>This class can be easily attached to the GridControl object in the following manner:</p>
+
+
+```xaml
+<dxg:GridControl x:Name="grid" AutoPopulateColumns="True">
+	<dxmvvm:Interaction.Behaviors>
+		<local:GridLayoutHelper LayoutChanged="OnGridLayoutChanged"/>
+	</dxmvvm:Interaction.Behaviors>
+</dxg:GridControl>
+```
+
+
+<p> </p>
+<p>To learn more on how to implement similar functionality in <strong>Silverlight</strong>, refer to the <a href="https://www.devexpress.com/Support/Center/p/T243422">T243422</a> example.</p>
+
+<br/>
+
+
